318716994d2964107a501cb759b1ef90ab02b788
João Iacillo <joaopiacillo@outlook.com.br>
2023-07-17 19:03:11 -0300

    Remove Maven

--- src/main/java/com/john/temperature/Temperature.java
+++
@@ -1,7 +0,0 @@
-package com.john.temperature;
-
-interface Temperature {
-	public double toCelsius(double value);
-	public double toFahrenheit(double value);
-	public double toKelvin(double value);
-}
